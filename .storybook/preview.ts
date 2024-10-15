90907fc7375e31b7b34bf0f415c434cded2a9a66
antoniowellington542 <antoniowellington542@gmail.com>
2024-10-15 10:15:30 -0300

    feat(*): adjust lint in files

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,15 @@
-import type { Preview } from "@storybook/react"
+import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
-    parameters: {
-        actions: { argTypesRegex: "^on[A-Z].*" },
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i
-            }
-        }
-    }
-}
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
 
-export default preview
+export default preview;
---
+++ lib/Button/index.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Button, ButtonProps } from "@/Button"
+
+const meta = {
+	title: "Components/Button",
+	component: Button,
+	parameters: {
+		layout: "centered"
+	},
+	tags: ["autodocs"],
+	argTypes: {
+		color: {
+			options: ["primary", "success", "info", "warning", "error"],
+			control: { type: "select" }
+		},
+		size: {
+			options: ["small", "medium", "large"],
+			control: { type: "select" }
+		},
+		variant: {
+			options: ["contained", "outlined", "text"],
+			control: {
+				type: "select"
+			}
+		}
+	}
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const defaultProps: ButtonProps = {
+	text: "Clique me",
+	color: "primary",
+	size: "medium",
+	variant: "contained",
+};
+
+export const DefaultButton: Story = {
+	args: {
+		...defaultProps,
+		children: "Click Me!",
+	},
+};
+
+export const DisabledButton: Story = {
+	args: {
+		...defaultProps,
+		children: "Click Me!",
+		disabled: true,
+	},
+};

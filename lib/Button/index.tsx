4b61ba15a9631091a88f22dcbce460093cf128f1
antoniowellington542 <antoniowellington542@gmail.com>
2024-10-15 09:59:50 -0300

    feat(*): adjust lint in files

--- lib/Button/index.tsx
+++ lib/Button/index.tsx
@@ -2,20 +2,20 @@ import React from "react"
 import { Button as MUIButton, ButtonOwnProps } from "@mui/material"
 
 export type ButtonProps = {
-    color: ButtonOwnProps["color"]
-    size: ButtonOwnProps["size"]
-    variant: ButtonOwnProps["variant"]
-    text: string
+	color: ButtonOwnProps["color"]
+	size: ButtonOwnProps["size"]
+	variant: ButtonOwnProps["variant"]
+	text: string
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-    return (
-        <MUIButton
-            color={props.color}
-            size={props.size}
-            variant={props.variant}
-        >
-            {props.text}
-        </MUIButton>
-    )
+	return (
+		<MUIButton
+			color={props.color}
+			size={props.size}
+			variant={props.variant}
+		>
+			{props.text}
+		</MUIButton>
+	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,22 +7,22 @@ import { peerDependencies } from "./package.json"
 import * as path from "path"
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./lib")
-        }
-    },
-    plugins: [react(), dts({ rollupTypes: true })],
-    build: {
-        target: "esnext",
-        minify: false,
-        lib: {
-            entry: resolve(__dirname, join("lib", "index.ts")),
-            fileName: "index",
-            formats: ["es"]
-        },
-        rollupOptions: {
-            external: ["react/jsx-runtime", ...Object.keys(peerDependencies)]
-        }
-    }
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./lib")
+		}
+	},
+	plugins: [react(), dts({ rollupTypes: true })],
+	build: {
+		target: "esnext",
+		minify: false,
+		lib: {
+			entry: resolve(__dirname, join("lib", "index.ts")),
+			fileName: "index",
+			formats: ["es"]
+		},
+		rollupOptions: {
+			external: ["react/jsx-runtime", ...Object.keys(peerDependencies)]
+		}
+	}
 })
